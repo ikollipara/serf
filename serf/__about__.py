0440961d0fa217de11465114d812341f3bc30e9d
Ian Kollipara <ian.kollipara@gmail.com>
2023-03-13 11:55:12 -0500

    :rocket: Initial Commit

---
+++ serf/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2023-present Ian Kollipara <ian.kollipara@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+__version__ = '0.0.1'
---
+++ serf/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2023-present Ian Kollipara <ian.kollipara@gmail.com>
+#
+# SPDX-License-Identifier: MIT
